7b5020d94891c07315bcfc386fba0e1dd0e90c38
Your Name <you@example.com>
2021-07-23 22:29:16 +0200

    seventh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-<<<<<<< HEAD
-        out.println("This is my first github commit");
-=======
         System.out.println("This is my first github commit second");
->>>>>>> dev
+
     }
 }
