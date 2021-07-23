487b24aa8300ec9c4a237496cb998b5e3a7eb173
Your Name <you@example.com>
2021-07-23 22:21:16 +0200

    fourth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,12 @@
 package com.company;
 
+import static java.lang.System.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        System.out.println("This is my first github commit");
+        out.println("This is my first github commit");
     }
 }
